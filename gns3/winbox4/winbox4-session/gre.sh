0377a503f4b30c604ca7ee7c9d4706c61794fa32
selene <selene@selene.selene>
2026-03-10 13:12:14 +0700

    delete gns3/

--- gns3/winbox4/winbox4-session/gre.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p session-file
-PROFILE="session-file/gre"
-
-mkdir -p "$PROFILE"
-
-HOME="$PROFILE" /home/selene/debianthinkpad/gns3/winbox4/winbox4
--- gns3/winbox4/winbox4-session/ipip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p session-file
-PROFILE="session-file/ipip"
-
-mkdir -p "$PROFILE"
-
-HOME="$PROFILE" /home/selene/debianthinkpad/gns3/winbox4/winbox4
