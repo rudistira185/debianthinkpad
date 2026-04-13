7f4b3d202ea71170d6d3742f0d668e928d7d9fe4
rudistira185 <rudistira185@gmail.com>
2026-04-14 03:35:14 +0700

    update linux/hotspot/2-start.sh

--- linux/hotspot/2-start.sh
+++ linux/hotspot/2-start.sh
@@ -1,2 +1,3 @@
 rm -rf linux-wifi-hotspot
-sudo create_ap wlp4s0 wlp4s0 debian 87654321 -g 172.25.25.1
+#sudo create_ap wlp4s0 wlp4s0 debian 87654321 -g 172.25.25.1
+sudo create_ap -c 10 wlp4s0 wlp4s0 debian 87654321 -g 172.25.25.1
