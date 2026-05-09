6c2dcdea7d57bb7356155101cb2c43a7f23cb331
rudistira185 <rudistira185@gmail.com>
2026-05-09 23:45:40 +0700

    update networks-labs/random-network/interkoneksi-4provinsi-gre

---
+++ network-labs/random-networks/interkoneksi-4provinsi-gre/docker-install.sh
@@ -0,0 +1,24 @@
+# add repo
+# # Add Docker's official GPG key:
+sudo apt update -y
+sudo apt install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+sudo apt update  -y
+
+# install docker
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# groups
+sudo usermod -aG docker $USER
